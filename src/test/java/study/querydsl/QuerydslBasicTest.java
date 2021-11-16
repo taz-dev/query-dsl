7fb48f7f3b8254eba8b6bebd1c7353b69e2adf99
taz-dev <tazdev@naver.com>
2021-11-16 23:05:09 +0900

    test: 조인(기본 조인)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -225,4 +225,40 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35); //(30 + 40) / 2
     }
+
+    //조인
+    //조인의 기본 문법은 첫 번째 파라미터에 조인 대상을 지정하고,
+    //두 번째 파라미터에 별칭으로 사용할 Q타입을 지정한다
+    //ex) 팀 A에 소속된 모든 회원
+   @Test
+    public void join() {
+       List<Member> result = queryFactory
+               .selectFrom(member)
+               .join(member.team, team)
+               .where(team.name.eq("teamA"))
+               .fetch();
+
+       assertThat(result)
+               .extracting("username")
+               .containsExactly("member1", "member2");
+   }
+
+   //세타 조인(연관관계가 없는 필드로 조인)
+   //ex) 회원의 이름이 팀 이름과 같은 회원 조회
+  @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+      List<Member> result = queryFactory
+              .select(member)
+              .from(member, team) //from절에 여러 엔티티를 선택해서 세타조인(외부 조인 불가능)
+              .where(member.username.eq(team.name))
+              .fetch();
+
+      assertThat(result)
+              .extracting("username")
+              .containsExactly("teamA", "teamB");
+  }
 }
