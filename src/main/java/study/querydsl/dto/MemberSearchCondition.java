51561c1c7f8e158b5d17634a42ec0265ae021b78
taz-dev <tazdev@naver.com>
2021-11-27 23:19:00 +0900

    test: 동적 쿼리와 성능 최적화 조회(Where절 파라미터 사용)

--- src/main/java/study/querydsl/dto/MemberSearchCondition.java
+++ src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class MemberSearchCondition {
     //회원명, 팀명, 나이(ageGoe, ageLoe)
-    private String userName;
+    private String username;
     private String teamName;
     private Integer ageGoe;
     private Integer ageLoe;
